6deb82740c75bdac89d4fb3abd7a21a5b37e8140
AissatouAM <jopmarieaissatou@gmail.com>
2026-06-01 21:27:19 +0000

    Modification du navbar et creation de About

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import "../styles/About.css"
+import me from "../assets/images/me.jpeg"
+
+function About() {
+  return (
+    <div id="about">
+      <div className="allabout">
+        <h1 className="aboutme">About me</h1>
+
+        <div className="paragraph">
+          <p>Second-year student in the computer science DUT program</p>
+          <p>At Polytechnic School of Dakar,</p>
+          <p>I'm looking to join a dynamic organization</p>
+          <p>Where I can gain professional experience,</p>
+          <p>deepen my skills in programming, machine learning and cybersecurity</p>
+          <p>And actively contribute to real-world projects.</p>
+        </div>
+      </div>
+      
+      <div className="photo-container">
+        <img className="maphoto" src={me} alt="maphoto" />
+      </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,15 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import About from "./About"
 
 function App() {
   return (
     <div className="App">
       
       <Navbar />
-
-      <section id="home">
-        <Home />
-      </section>
-
-      <section id="about">
-        
-      </section>
-
-      <section id="projects">
-        
-      </section>
-
-      <section id="blog">
-        
-      </section>
-
-      <section id="contact">
-        
-      </section>
-
-
+      <Home />
+      <About />
+      
     </div>
   );
 }

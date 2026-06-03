72b4b745d8b64d4acad9a1fc2896d60789f1f707
AissatouAM <jopmarieaissatou@gmail.com>
2026-06-03 15:11:41 +0000

    Remise en place de la photo

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ function About() {
   return (
     <div id="about" >
       <div className="allabout">
+
         <h1 className="aboutme" data-aos="fade-left">About me</h1>
 
         <div className="paragraph" data-aos="fade-up" data-aos-delay="300">
@@ -19,9 +20,14 @@ function About() {
           <p data-aos="fade-up">I enjoy working on practical projects that help me apply what I learn .</p>
           <p data-aos="fade-up">I am highly motivated to grow, improve, and contribute to impactful technological solutions.</p>
         </div>
+        
       </div>
       
+      <div className="photo">
+
         <img className="maphoto" src={me} alt="maphoto" data-aos="flip-right" data-aos-delay="400"/>
+
+      </div>
     
     </div>
   )
